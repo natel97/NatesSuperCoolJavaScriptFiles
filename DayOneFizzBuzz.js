e63dc5ba62f3e5f585733be0d06f0dce2a48b2fb
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-10-24 15:28:11 -0500

    Finished day one FizzBuzz project

---
+++ DayOneFizzBuzz.js
@@ -0,0 +1,19 @@
+let _=require('lodash')
+
+//Fizbuzz itself
+let fizzBuzz = (input) => {
+  return (input % 5 == 0) ?
+    (input % 3 == 0) ?
+        input + ": FIZZBUZZ!!!" : input + ": Buzz"
+    : (input % 3 == 0) ?
+      input + ": Fizz" : null
+}
+
+//getFizzBuzzable variables from array
+let fizzBuzzFilteredArray = (arr) => _.filter(arr, function(o) { return (fizzBuzz(o) != null) })
+
+//Pair values with words!
+let fizzBuzzArray = (arr) => _.map(fizzBuzzFilteredArray(arr),fizzBuzz)
+
+//---->Run the thing!<----
+console.log(fizzBuzzArray(_.range(1,100)))
